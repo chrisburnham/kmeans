07c3eeb7cb535d047ffb7a92812b3dfba3eecf1b
Chris Burnham <cburnham@greensea.com>
2019-05-05 10:30:57 -0400

    adding initial file

---
+++ kmeans.py
@@ -0,0 +1,4 @@
+# Randomly pick k centroids
+# Assign each data point to nearest centroid
+# Calculate centroid of each cluster (mean of points)
+# Repeate until no points change or a max iteration is reached

4ba6ea581e98b54d0cf61e93cc5f7bf0f31fbae3
Chris Burnham <cburnham@greensea.com>
2019-05-12 21:33:00 -0400

    clustering

--- kmeans.py
+++ kmeans.py
@@ -72,7 +72,7 @@ def assign_points(data, centroids):
 	for point in data:
 		min_dist = 99999
 		pt_idx = -1
-		for i in len(centroids):
+		for i in range(len(centroids)):
 			dist = calculate_distance(centroids[i], point)
 			if(dist < min_dist):
 				min_dist = dist
@@ -86,8 +86,9 @@ def assign_points(data, centroids):
 
 # Calculate a new centroid based off of the collected points
 def calculate_centroid(data_points):
-	matrix_sum = numpy.sum(data_points)
+	matrix_sum = numpy.sum(data_points, axis=0)
 	matrix_sum = matrix_sum / len(data_points)
+
 	return matrix_sum
 
 ###########################################################
@@ -97,25 +98,27 @@ def calculate_centroid(data_points):
 # this has run
 # Runs untils clusters don't move or max runs
 def run_cluster(data, centroids, runs):
-	print "Run number" + str(runs)
+	print "Run number " + str(runs)
+
+	print centroids
 
 	sorted_data = assign_points(data, centroids)
-	new_centroids = len()
+	new_centroids = list()
 	same_centroids = True
 	for cluster in sorted_data:
 		new_cen = calculate_centroid(cluster)
 		new_centroids.append(new_cen)
 		found = False
 		for old_cen in centroids:
-			if(new_cen == old_cen):
+			#if(new_cen == old_cen):
+			if(numpy.array_equal(new_cen, old_cen)):
 				found = True
 				break
 
-		if(found):
-			same_centroids = False
+		same_centroids = same_centroids and found
 
 	# TODO: get max_runs from arg
-	max_runs = 5
+	max_runs = 50
 
 	if(same_centroids or (runs > max_runs)):
 		print "Clustering done"
@@ -200,13 +203,16 @@ if __name__ == "__main__":
 	args = vars(parser.parse_args())
 
 	data, headers = read_csv(args.get("data_file"))
-	num_clusters = args.get("cols")
+	num_clusters = args.get("clusters")
+
+	print data
 
 	rand = random.Random()
 	rand.seed()
 	initial_centroids = list()
 	for i in range(num_clusters):
-		rand_num = rand.uniform(0, data.size[0])
+		rand_num = rand.uniform(0, 100)
+		#rand_num = rand.uniform(0, data.size[0])
 		initial_centroids.append(data[rand_num, :])
 
 	run_cluster(data, initial_centroids, 1)
